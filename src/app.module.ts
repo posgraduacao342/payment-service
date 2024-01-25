7179846ed30e715039a59cb9d872b25d63485a91
matheusGolmini <matheusgolminif@gmail.com>
2024-01-25 14:03:51 -0300

    refactor: change module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PagamentoModule } from './application/config/PagamentoModule';
+import { ApplicationModule } from './application/ApplicationModule';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    PagamentoModule,
+    ApplicationModule,
     MongooseModule.forRoot('mongodb://mongodb:27017/test'),
   ],
 })
--- src/application/ApplicationModule.ts
+++ src/application/ApplicationModule.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
-import { PagamentoController } from '../controllers/PagamentoController';
+import { PagamentoController } from './controllers/PagamentoController';
 import { ProcessarPagamentoUseCase } from 'src/domain/useCases/ProcessarPagamentoUseCase';
 import { PagamentoGatewayPortKey } from 'src/domain/ports/out/PagamentoGatewayPort';
-import { PagamentoGateway } from '../gateway/PagamentoGateway';
+import { PagamentoGateway } from './gateway/PagamentoGateway';
 import { MetodoPagamentoFactory } from 'src/domain/factories/MetodoPagamentoFactory';
 import { MercadoPagoQRCodeStrategy } from 'src/domain/strategies/MercadoPagoQRCodeStrategy';
 import { DinheiroStrategy } from 'src/domain/strategies/DinheiroStrategy';
 import { MercadoPagoGatewayPortKey } from 'src/domain/ports/out/MercadoPagoGatewayPort';
-import { MercadoPagoGateway } from '../gateway/MercadoPagoGateway';
+import { MercadoPagoGateway } from './gateway/MercadoPagoGateway';
 import { ObterPagamentosUseCase } from 'src/domain/useCases/ObterPagamentosUseCase';
 import { MercadoPagoModule } from 'src/infrastructure/mercadoPago/MercadoPagoModule';
 import { MongoModule } from 'src/infrastructure/db/MongoModule';
 import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from '../controllers/HealthController';
+import { HealthController } from './controllers/HealthController';
 import { HttpModule } from '@nestjs/axios';
 import { ValidarPagamentoMPUseCase } from 'src/domain/useCases/ValidarPagamentoMPUseCase';
-import { PagamentoRepository } from 'src/infrastructure/db/repositories/PagamentoRepository';
 import { ObterPagamentoPorPedidoIdUseCase } from 'src/domain/useCases/ObterPagamentoPorPedidoIdUseCase';
 
 @Module({
@@ -27,7 +26,6 @@ import { ObterPagamentoPorPedidoIdUseCase } from 'src/domain/useCases/ObterPagam
     MercadoPagoQRCodeStrategy,
     DinheiroStrategy,
     ObterPagamentosUseCase,
-    PagamentoRepository,
     ValidarPagamentoMPUseCase,
     ObterPagamentoPorPedidoIdUseCase,
     {
@@ -40,4 +38,4 @@ import { ObterPagamentoPorPedidoIdUseCase } from 'src/domain/useCases/ObterPagam
     },
   ],
 })
-export class PagamentoModule {}
+export class ApplicationModule {}
